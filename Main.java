2671f7df236a58a337303cd20affd5d508cdcc2b
amaiabrea <amaiabrea@gmail.com>
2024-11-27 21:03:24 +1000

    initialized scanner

--- Main.java
+++ Main.java
@@ -5,18 +5,23 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-
+import java.io.*;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main
 {
     public static void main (String[] args){
+        
+        Scanner scanner=new Scanner(System.in);
         
     try{
         System.out.print("Enter the file name: ");
         String fileName=scanner.nextLine();
     
     
-        }catch(IOException e){
+        }catch(Exception e){
         System.out.println("Error reading the file");
         return;
         }
