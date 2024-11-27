3c91beac7955e1d67f9e687bd5423982b0abdfc6
amaiabrea <amaiabrea@gmail.com>
2024-11-27 18:56:42 +1000

    user gives the file name and the programm reads the file

--- Main.java
+++ Main.java
@@ -5,29 +5,21 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+
+import java.util.Scanner;
 public class Main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    public static void main (String[] args){
+        
+    try{
+        System.out.print("Enter the file name: ");
+        String fileName=scanner.nextLine();
+    
+    
+        }catch(IOException e){
+        System.out.println("Error reading the file");
+        return;
+        }
 }
+}
+
