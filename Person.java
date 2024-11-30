25bf0c14c10c2b5beb74e40bd2d96c4b21bf8950
amaiabrea <amaiabrea@gmail.com>
2024-11-30 12:04:44 +1000

    getter method for the name

--- Person.java
+++ Person.java
@@ -7,5 +7,21 @@
  */
 public class Person
 {
-  
+ private String name;
+ private String id;
+ 
+ 
+ 
+ public Person(String name, String id){
+    this.name=name;
+    this.id=id;    
+    
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    }
+    
 }
