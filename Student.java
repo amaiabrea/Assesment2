cfe656341d7e42905874dab819686e0b5e6bc298
amaiabrea <amaiabrea@gmail.com>
2024-11-30 12:10:28 +1000

    call the constructor of Person

--- Student.java
+++ Student.java
@@ -8,4 +8,13 @@
 public class Student extends Person
 {
    private int mark1, mark2, mark3, totalMarks;
+   
+   
+   public Student(String name, String id, int mark1, int mark2, int mark3){
+    
+   super(name, id); 
+    
+    
+    
+    }
 }
