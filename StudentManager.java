39701ada3b55dd9ea7f91a1db00fc3c49a94b89b
amaiabrea <amaiabrea@gmail.com>
2024-12-02 11:45:35 +1000

    swap

--- StudentManager.java
+++ StudentManager.java
@@ -54,8 +54,17 @@ public class StudentManager implements Sortable
 
 @Override
 public void sortDescending(List<Student> students){
-int n=students.size();
-
+    int n=students.size();
+    for(int i=0; i<n-1;i++){
+        for (int j=0; j<n-i; j++){
+            if(students.get(j).getTotalMarks() < students.get(j +1).getTotalMarks()){
+                Student temp = students.get(j);
+                students.set(j, students.get(j + 1));
+                students.set(j + 1, temp);
+            
+            }
+        }
+        }    
 
 
 }
